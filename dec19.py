0b0813c91447c8088f5f29ec7662eec4373e3225
kat <kathryn.baker@stfc.ac.uk>
2024-12-19 11:05:25 +0000

    dec19

---
+++ dec19.py
@@ -0,0 +1,42 @@
+from collections import Counter
+
+available_string = "r, wr, b, g, bwu, rb, gb, br"
+available = ["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]
+
+looking_for = "brwrr"
+
+chars_available = Counter(available_string).keys()
+
+target = len(looking_for)
+count = 0
+for char in looking_for:
+    print(char)
+    if char not in chars_available:
+        print("Nope, not here, this is impossible")
+        break
+    if char in available:
+        print("Available on own")
+        count += 1
+
+print(count)
+
+if count == target:
+    print("Possible, great")
+else:
+    print("Need to go to next level")
+
+    check_len = 1
+    count = 0
+    for i in range(target):
+        if looking_for[i:i+check_len] in available:
+            print(f"I have {looking_for[i:i+check_len]} available")
+            count += check_len
+        else:
+            print(f"{looking_for[i:i+check_len]} isn't available")
+    print(f"I've gotten to a length of {count} with {check_len}")
+
+
+    check_len = 2
+    if looking_for[:check_len] in available:
+        print("Got it, repeat for remaining string")
+
