44c6c9b51f29caeebf1da2ea8e25a741ae455361
kat <kathryn.baker@stfc.ac.uk>
2024-12-23 01:27:56 +0000

    dec19

--- dec19.py
+++ dec19.py
@@ -10,40 +10,49 @@ max_available = 3
 looking_for = "brwrr"
 chars_looking_for = ["b", "r", "w"]
 
-chars_available = Counter(available_string).keys()
+available_chars_count = Counter(available_string)
+del available_chars_count[","]
+del available_chars_count[" "]
+chars_available = available_chars_count.keys()
 
-target = len(looking_for)
-count = 0
-for char in looking_for:
-    print(char)
-    if char not in chars_available:
-        print("Nope, not here, this is impossible")
-        break
-    if char in available:
-        print("Available on own")
-        count += 1
+print(available_chars_count)
+print(chars_available)
 
-print(count)
+for thing in reversed(available_chars_count.most_common()):
+    print(thing)
 
-if count == target:
-    print("Possible, great")
-else:
-    print("Need to go to next level")
-
-    check_len = 1
-    count = 0
-    for i in range(target):
-        if looking_for[i:i+check_len] in available:
-            print(f"I have {looking_for[i:i+check_len]} available")
-            count += check_len
-        else:
-            print(f"{looking_for[i:i+check_len]} isn't available")
-    print(f"I've gotten to a length of {count} with {check_len}")
-
-    # todo
-    # This is certainly a recursion type thing, I like having the quick check for any unavailable colours
-    # but I think I need to do the checks in reverse order, as the longer the better
-
-    check_len = max_available
-
-
+# target = len(looking_for)
+# count = 0
+# for char in looking_for:
+#     print(char)
+#     if char not in chars_available:
+#         print("Nope, not here, this is impossible")
+#         break
+#     if char in available:
+#         print("Available on own")
+#         count += 1
+#
+# print(count)
+#
+# if count == target:
+#     print("Possible, great")
+# else:
+#     print("Need to go to next level")
+#
+#     check_len = 1
+#     count = 0
+#     for i in range(target):
+#         if looking_for[i:i+check_len] in available:
+#             print(f"I have {looking_for[i:i+check_len]} available")
+#             count += check_len
+#         else:
+#             print(f"{looking_for[i:i+check_len]} isn't available")
+#     print(f"I've gotten to a length of {count} with {check_len}")
+#
+#     # todo
+#     # This is certainly a recursion type thing, I like having the quick check for any unavailable colours
+#     # but I think I need to do the checks in reverse order, as the longer the better
+#
+#     check_len = max_available
+#
+#
